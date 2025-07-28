159e8a33a352e0aa4528a3119e35adb8bbd887f5
Chelike Nikhil <100200794+nikhil32047@users.noreply.github.com>
2025-07-28 13:01:24 +0530

    Update Customer.js

--- Customer.js
+++ Customer.js
@@ -1,8 +1,55 @@
 "Functional Component"
+
+"Functional Component(NameFull function)"
 function Customer()
 {
     return(
-        <h1>Welcome to Functional Component</h1>
+        <h1>Welcome to Functional Component...</h1>
     )
 }
 export default Customer;
+
+
+"Functional Component(Name_Less function)"
+var Customer=function()
+{
+    return(
+        <h1>Using Name_Less function...</h1>
+    )
+}
+export default Customer;
+
+
+"Functional Component(Name_Less function)"
+var Customer=()=>
+{
+    return(
+        <h1>Using Arrow function...</h1>
+    )
+}
+export default Customer;
+
+
+
+
+"Class Component"
+
+import React from "react"
+class Customer extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Using Class Component...</h1>
+            </div>
+        )
+    }
+}
+export default Customer;
+
+
+
+
+
+
